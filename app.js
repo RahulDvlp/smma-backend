2ea9c701ca7cc5a746c8cd1fd381b58423a0ab6e
Rahul <100018044+RahulDvlp@users.noreply.github.com>
2023-02-28 18:23:51 +0530

    attempt3

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.listen(port, () => {
-  console.log("server started on port " + port);
+app.listen(PORT, () => {
+  console.log("server started on port " + PORT);
 });
